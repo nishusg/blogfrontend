ec978cc07bf01828a3c2f90ac6b85dcb30664885
NISHANT <51988633+nishusg@users.noreply.github.com>
2021-12-31 16:46:31 +0530

    Update nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -6,8 +6,6 @@ const Nav = ({ blogs }) => {
   const { length: count } = blogs;
   return (
     <nav className="navbar navbar-expand-lg sticky-top container  navbar-light bg-light">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1528115798345167"
-     crossorigin="anonymous"></script>
       <div className="container-fluid">
         <span className="navbar-brand mb-0 h1">Navbar {count}</span>
         <ul className="nav justify-content-end">
